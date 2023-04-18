b8ae36fb5129393cf9cb4e10987079e067a6f897
Sagrario Meneses <sagrario.meneses@gmail.com>
2023-04-17 23:01:05 -0600

    [PS] Module 1

---
+++ java-pluralsight/src/com/sag/example/Main.java
@@ -0,0 +1,18 @@
+package com.sag.example;
+
+/**
+ * Main class contains all the exercises
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("First line from app"); // This is a line comment
+        System.out.println( "Second line from app" ) ;
+        System.out.println(       "Third line from app" );
+    }
+
+    // This is my comment
+
+
+}
